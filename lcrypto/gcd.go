b7133d06ba740fd4b43e62203683fcf4d092ec7a
Calana2 <kalcast00@proton.me>
2024-12-06 13:22:51 -0500

    base64 decoder completed, calls to fmt changed by writes to an io.Writer

--- lcrypto/gcd.go
+++ lcrypto/gcd.go
@@ -1,9 +1,9 @@
+// Euclides algorithm
 package lcrypto
 
 import "math/big"
 
 func GCD(a *big.Int, b *big.Int) *big.Int {
- // Euclides algorithm
  for b.Cmp(big.NewInt(0)) != 0 {
    tmp := new(big.Int).Set(b)
    b = new(big.Int).Mod(a,b)
